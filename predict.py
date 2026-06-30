3431283d7c7a567fd7a5673b3a930ac2147ed044
Nikhitha Gummala <gummalanikhitha@gmail.com>
2026-06-30 14:12:42 +0530

    Refactor predict_image function and model loading

--- predict.py
+++ predict.py
@@ -1,13 +1,32 @@
 import numpy as np
-from tensorflow.keras.models import load_model
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout
+from tensorflow.keras.models import Model
+
 from utils import preprocess_image
 
-MODEL_PATH = "model/cable_model.keras"
+# Build the same model architecture
+base_model = MobileNetV2(
+    weights="imagenet",
+    include_top=False,
+    input_shape=(224, 224, 3)
+)
 
-model = load_model(MODEL_PATH)
+base_model.trainable = False
+
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dropout(0.3)(x)
+x = Dense(128, activation="relu")(x)
+output = Dense(1, activation="sigmoid")(x)
+
+model = Model(base_model.input, output)
+
+# Load weights
+model.load_weights("model/cable_model.weights.h5")
+
 
 def predict_image(image):
-
     img = preprocess_image(image)
 
     prediction = model.predict(img, verbose=0)[0][0]
@@ -15,10 +34,6 @@ def predict_image(image):
     confidence = round(max(prediction, 1 - prediction) * 100, 2)
 
     if prediction >= 0.5:
-        label = "Good"
-        recommendation = "Cable is safe to use."
+        return "Good", confidence, "Cable is safe to use."
     else:
-        label = "Damaged"
-        recommendation = "Replace or inspect the cable."
-
-    return label, confidence, recommendation
+        return "Damaged", confidence, "Replace or inspect the cable."
